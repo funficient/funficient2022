9f19df1bbffbb338170a2ae144c0cfc9f1bd857d
karindames <karin.dames@hotmail.com>
2025-01-02 22:51:24 +0200

    Move to right location

--- cards.js
+++ cards.js
@@ -188,26 +188,20 @@ var deck = [
 
     new card('Monster', 'monster'),
     new card('Fox', 'fox'),
+    new card('Tie', 'tie'),
     new card('Queen', 'queen')
 
 
 /*
     new card('Cancer', 'toast'),    new card('Tool', 'toast'),       
-    new card('Rag', 'toast'),      new card('Thumbtack', 'toast'),    new card('Tie', 'toast'),    new card('Jell-O', 'toast'),    new card('Pot', 'toast'),       new card('Hoe', 'toast'),    
+    new card('Rag', 'toast'),      new card('Thumbtack', 'toast'),        new card('Jell-O', 'toast'),    new card('Pot', 'toast'),       new card('Hoe', 'toast'),    
     new card('Lobby', 'toast'),    new card('Fulcrum', 'toast'),    new card('Rotating spit', 'toast'),    new card('Groundhog', 'toast'),    new card('Lungs', 'toast'),    new card('Speech', 'toast'),    new card('Burdock', 'toast'),    new card('Sludge', 'toast'),    new card('Wastebasket', 'toast'),    new card('Tavern', 'toast'),    new card('Screwdriver', 'toast'),    new card('VCR', 'toast'),    new card('Stereo', 'toast'),         new card('Christmas', 'toast'),    
     new card('Ditches', 'toast'),        new card('Razor', 'toast'),
 
-    new card('Eyedropper', 'toast'),
-    new card('Actor', 'toast'),
-    new card('Homeless', 'toast'),
-   
-    new card('Artist', 'toast'),
-    new card('Storm', 'toast'),
-    new card('Indian', 'toast'),
-    new card('Snake', 'toast'),
-
-
-    new card('Satan', 'toast'),
+    new card('Eyedropper', 'toast'),    new card('Actor', 'toast'),
+    new card('Homeless', 'toast'),    new card('Artist', 'toast'),
+    new card('Storm', 'toast'),    new card('Indian', 'toast'),
+    new card('Snake', 'toast'),    new card('Satan', 'toast'),
 
     
     new card('Acne', 'toast'),
